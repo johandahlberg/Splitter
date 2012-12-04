3a1ff51a1986a8d882278c582cc1e93734c33966
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2012-12-04 11:42:08 +0100

    Removing unused imports

--- test/packages/se/uu/medsci/splitter/SimpleFileSplitterTest.java
+++ test/packages/se/uu/medsci/splitter/SimpleFileSplitterTest.java
@@ -2,14 +2,6 @@ package se.uu.medsci.splitter;
 
 import static org.junit.Assert.*;
 
-import net.sf.picard.fastq.FastqRecord;
-
-import org.junit.Test;
-import java.io.File;
-import net.sf.picard.fastq.*;
-import se.uu.medsci.splitter.fastq.*;
-import se.uu.medsci.splitter.*;
-
 public class SimpleFileSplitterTest {
 
 	@Test
--- test/packages/se/uu/medsci/splitter/SplitterFastqFileWriterFactoryTest.java
+++ test/packages/se/uu/medsci/splitter/SplitterFastqFileWriterFactoryTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import java.io.File;
-
 public class SplitterFastqFileWriterFactoryTest {
 
 	@Test
